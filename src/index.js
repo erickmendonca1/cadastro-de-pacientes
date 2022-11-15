d2ecbc7a0e1b7a5068b1616064c2fc47097eae6d
Erick Mendonça <erickm.uff@gmail.com>
2022-11-14 21:51:16 -0300

    Criação de tabela no index.html e teste de inserção no index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+alert("Iniciaremos o cadastro do paciente!")
+//Apenas testando a inserção de dados
+let primeiroNome = prompt("Insira o seu primeiro nome: ")
+let sobrenome = prompt("Insira o seu sobrenome: ")
+let cpf = prompt("Insira o CPF do paciente: ")
+let idade = prompt("Insira idade do paciente: ")
+
+idade=parseInt(idade)
+
+console.log(primeiroNome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(idade)
